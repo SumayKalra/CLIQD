4b90d00ccba0a40bdd8fa4e1a12d85ff1f97d1bc
Sumay Kalra <sumaykalra@gmail.com>
2025-04-15 15:17:24 -0700

    Xcode integration

---
+++ CLIQ/CLIQ/CLIQApp.swift
@@ -0,0 +1,17 @@
+//
+//  CLIQApp.swift
+//  CLIQ
+//
+//  Created by Sumay Kalra on 4/15/25.
+//
+
+import SwiftUI
+
+@main
+struct CLIQApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
